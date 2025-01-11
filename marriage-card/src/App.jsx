72b6e4e16ed05f0208de38343b6298c02dd30b16
Aaryan Dhakal <aaryandhakal2020@gmail.com>
2025-01-11 21:37:14 +0545

    Add marriage card component

--- marriage-card/src/App.jsx
+++ marriage-card/src/App.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import CreateNew from "./components/CreateNew";
 import CreateCard from "./components/CreateCard";
-
+import MarriageCard from "./components/MarriageCard";
 const App = () => {
   const [isHome, setIsHome] = useState(true);
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-blue-100">
-      {isHome ? (
+      {/* {isHome ? (
         <CreateNew setIsHome={setIsHome} />
       ) : (
         <CreateCard setIsHome={setIsHome} />
-      )}
+      )} */}
+      <MarriageCard />
     </div>
   );
 };
---
+++ marriage-card/src/components/MarriageCard.jsx
@@ -0,0 +1,46 @@
+import { Heart } from "lucide-react";
+import React from "react";
+import { Card, CardContent } from "./ui/card";
+
+const MarriageCard = () => {
+  return (
+    <div className="flex flex-col items-center gap-7 text-center">
+      <Card className="shadow-lg max-w-xs md:max-w-2xl">
+        <CardContent className="p-5 space-y-5 font-serif">
+          <div className="flex items-center gap-3">
+            <div className="w-full h-0.5 bg-gray-300"></div>
+            <Heart size={32} color="red" className="flex-shrink-0" />
+            <div className="w-full h-0.5 bg-gray-300"></div>
+          </div>
+          <div className="font-medium text-3xl">Getting Married</div>
+          <div className="flex flex-col md:flex-row items-center gap-7 justify-center">
+            <div className="space-y-1">
+              <img
+                className="w-48 h-60 rounded-lg object-cover"
+                src="https://wallpapers.com/images/high/confused-patrick-random-pfp-x63wp9vs43cem64s.webp"
+                alt=""
+              />
+              <p className="text-lg font-medium">Apsani Budhathoki</p>
+            </div>
+            <Heart size={50} color="red" fill="red" />
+            <div className="space-y-1">
+              <img
+                className="w-48 h-60 rounded-lg object-cover"
+                src="https://wallpapers.com/images/high/confused-patrick-random-pfp-x63wp9vs43cem64s.webp"
+                alt=""
+              />
+              <p className="text-lg font-medium">Aaryan Dhakal</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="w-96 h-0.5 bg-gray-300"></div>
+            <Heart size={32} color="red" className="flex-shrink-0" />
+            <div className="w-96 h-0.5 bg-gray-300"></div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default MarriageCard;
