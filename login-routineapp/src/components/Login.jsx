bcb1cc467928909cefcbf6cbff7a80cabdf478b4
Aaryan Dhakal <aaryandhakal2020@gmail.com>
2025-02-13 16:03:39 +0545

    Improve UI

--- login-routineapp/src/components/Login.jsx
+++ login-routineapp/src/components/Login.jsx
@@ -39,17 +39,21 @@ const Login = () => {
             Login
           </Button>
           <div className="flex items-center justify-evenly text-gray-400 w-full my-5">
-            <div className="flex-shrink-0 h-0.5 w-[30%] bg-gray-300"></div>
-            <p className="w-full">OR Login With</p>
-            <div className="flex-shrink-0 h-0.5 w-[30%] bg-gray-300"></div>
+            <div className="flex-shrink-0 h-0.5 w-1/3 bg-gray-300"></div>
+            <p className="w-full">OR</p>
+            <div className="flex-shrink-0 h-0.5 w-1/3 bg-gray-300"></div>
           </div>
           <CardFooter>
-            <a href="" className="p-3 shadow-[0_1px_3px_gray] rounded-xl active:scale-90 transition">
+            <a
+              href=""
+              className="p-3 shadow-[0_1px_3px_gray] rounded-xl active:scale-90 transition flex items-center gap-3"
+            >
               <img
                 className="w-5 h-5"
                 src="https://cdn.iconscout.com/icon/free/png-512/free-google-logo-icon-download-in-svg-png-gif-file-formats--youtube-pack-logos-icons-1721659.png?f=webp&w=256"
                 alt="google"
               />
+              <p className="text-gray-500 text-sm">Login with Google</p>
             </a>
           </CardFooter>
         </Card>
